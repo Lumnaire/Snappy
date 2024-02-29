d63bf02ef1627111c1fa8f25898f9290c365611c
Lumnaire <callme14noy@gmail.com>
2024-02-29 15:33:08 +0800

    fixing editPost and Fileuploader

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -3,17 +3,10 @@ import { useParams } from "react-router-dom";
 import PostForm from "@/components/forms/PostForm";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import Loader from "@/components/shared/Loader";
-import { useEffect } from "react";
 
 const EditPost = () => {
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || "");
-
-  // useEffect(() => {
-  //   console.log(id);
-  //   console.log(post);
-  // }, [post]);
-  // This is used to check
 
   if (isPending)
     return (
